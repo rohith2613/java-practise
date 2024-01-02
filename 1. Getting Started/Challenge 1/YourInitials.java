02a34a6457e1f7bb4c2927f541170d9553e414d6
rohithsriramula <154052002+rohith2613@users.noreply.github.com>
2024-01-02 19:59:05 +0530

    Add files via upload

---
+++ 1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,16 @@
+
+/**
+ * YourInitials
+ */
+public class YourInitials {
+
+    public static void main(String[] args) {
+        System.out.println("  SSS     A   ");
+        System.out.println(" S   S   A A  ");
+        System.out.println(" S      A   A ");
+        System.out.println("  SSS   AAAAA ");
+        System.out.println("     S  A   A ");
+        System.out.println(" S   S  A   A ");
+        System.out.println("  SSS   A   A ");
+    }
+}
